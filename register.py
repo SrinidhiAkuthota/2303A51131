b8b1b28db57a7e195487dd272ef7eb3ad0102262
SrinidhiAkuthota <srinidhiakuthota24@gmail.com>
2026-06-18 13:50:27 +0530

    Add files via upload

--- register.py
+++ register.py
@@ -2,16 +2,16 @@ import requests
 
 url = "http://4.224.186.213/evaluation-service/register"
 
-data = {
-    "email": "srinidhiakuthota24@gmail.com",
-    "name": "Akuthota Srinidhi",
+payload = {
+    "email": "srinidhiakuthota@24gmail.com",
+    "name": "Srinidhi Akuthota",
     "mobileNo": "7989449412",
     "githubUsername": "SrinidhiAkuthota",
     "rollNo": "2303A51131",
     "accessCode": "bDreAq"
 }
 
-response = requests.post(url, json=data)
+response = requests.post(url, json=payload)
 
-print("Status Code:", response.status_code)
-print("Response:", response.text)
+print(response.status_code)
+print(response.json())
